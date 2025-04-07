3da9b3032aa4f1e85417eaf1722236d52ada356c
Amit Dange <amitsuresh_dange@epam.com>
2025-04-07 20:10:58 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,55 +6,29 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Experience from "@/components/Experience";
 import ContactMe from "@/components/Contact";
-import Loading from "./Loading";
 
-// Alternative 2: Class Component Approach
-import React, { Component } from 'react';
-
-class App extends Component {
-  state = {
-    isLoading: true
-  }
-
-  componentDidMount() {
-    // Simulating content load
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 1000);
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <div style={{ 
-          textAlign: 'center', 
-          paddingTop: '50vh' 
-        }}>
-          Loading...
-        </div>
-      );
-    }
-
-    return (
-      <>
-        <section id="hero">
-          <Hero />
-        </section>
-        <section id="about">
-          <About />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-        <section id="experience">
-          <Experience />
-        </section>
-        <section id="contact">
-          <ContactMe />
-        </section>
-      </>
-    );
-  }
+export default function Home() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <section id="hero">
+        <Hero />
+      </section>
+      {/* <section id="skills">
+        <Skills />
+      </section> */}
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="experience">
+        <Experience />
+      </section>
+      <section id="contact">
+        <ContactMe />
+      </section>
+    </div>
+  );
 }
-
-export default App;
